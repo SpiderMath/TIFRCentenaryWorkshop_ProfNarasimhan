7a558ac508d7f790f3410aeebd2c834071e36764
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2026-09-18 00:28:19 +0530

    update notices

--- js/notices.js
+++ js/notices.js
@@ -3,18 +3,18 @@
     // Set "active" to true/false to turn an item on or off globally.
     // In "pages", set boolean flags to control on which pages the notice appears.
     const siteNotices = [
-        // {
-        //     id: "poster-extension",
-        //     tag: "Notice",
-        //     html: 'Poster submission deadline has been extended to 15 November 2026. <a href="participate.html#engagement-tracks">Submit your poster proposal</a>.',
-        //     active: true,
-        //     pages: {
-        //         home: true,
-        //         programme: true,
-        //         participate: true,
-        //         publicLecture: true
-        //     }
-        // }
+        {
+            id: "poster-extension",
+            tag: "Notice",
+            html: 'Poster submission deadline has been extended to 15 November 2026. <a href="participate.html#engagement-tracks">Submit your poster proposal</a>.',
+            active: true,
+            pages: {
+                home: true,
+                programme: true,
+                participate: true,
+                publicLecture: true
+            }
+        }
     ];
     const mountPoint = document.getElementById("notices-mount");
     if (!mountPoint) return;
