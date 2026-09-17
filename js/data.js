6c337466686254630be4c3072664dd3698f5b59a
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2026-09-17 18:54:00 +0530

    Added multiple talk ability in the page

--- js/data.js
+++ js/data.js
@@ -1,163 +1,186 @@
 const workshopData = {
-  schedule: [
-    {
-      day: "Thursday, 10 December 2026",
-      sessions: [
+    schedule: [
         {
-          time: "09:00 - 09:15",
-          title: "Welcome address",
-          speaker: "",
-          anchor: "",
-          isBreak: false
+            day: "Thursday, 10 December 2026",
+            sessions: [
+                {
+                    time: "09:00 - 09:15",
+                    title: "Welcome address",
+                    speaker: "",
+                    anchor: "",
+                    isBreak: false
+                },
+                {
+                    time: "09:15 - 12:00",
+                    title: "Algorithms & Formal Methods",
+                    speaker: "",
+                    anchor: "",
+                    isBreak: false,
+                    talks: [
+                        {
+                            time: "09:15 - 10:00",
+                            title: "Algorithms are intriguing",
+                            speaker: "Chandra Chekuri",
+                            anchor: "chandra-chekuri",
+                            abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                        },
+                        {
+                            time: "10:00 - 10:45",
+                            title: "Formally discussing algorithms",
+                            speaker: "Ashutosh Trivedi",
+                            anchor: "ashutosh-trivedi",
+                            abstract: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                        },
+                        {
+                            // time: "11:15 - 12:00",
+                            title: "Methods in formal things",
+                            speaker: "Rohit Gurjar",
+                            anchor: "rohit-gurjar",
+                            abstract: "Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris."
+                        }
+                    ]
+                },
+                {
+                    time: "12:00 - 13:30",
+                    title: "Lunch",
+                    speaker: "",
+                    anchor: "",
+                    isBreak: true
+                },
+                {
+                    time: "13:30 - 16:00",
+                    title: "Commemorative session on Prof. Narasimhan",
+                    speaker: "",
+                    anchor: "",
+                    isBreak: false
+                },
+                {
+                    time: "16:00 - 17:30",
+                    title: "Student poster session",
+                    speaker: "",
+                    anchor: "",
+                    isBreak: false
+                },
+                {
+                    time: "17:30 - 19:00",
+                    title: "Public lecture: Trustworthy AI for Clinical Decision Making",
+                    speaker: "Prof. Rajeev Alur",
+                    anchor: "rajeev-alur",
+                    isBreak: false
+                }
+            ]
         },
         {
-          time: "09:15 - 12:00",
-          title: "Algorithms & Formal Methods",
-          speaker: "",
-          anchor: "",
-          isBreak: false
-        },
-        {
-          time: "12:00 - 13:30",
-          title: "Lunch",
-          speaker: "",
-          anchor: "",
-          isBreak: true
-        },
-        {
-          time: "13:30 - 16:00",
-          title: "Commemorative session on Prof. Narasimhan",
-          speaker: "",
-          anchor: "",
-          isBreak: false
-        },
-        {
-          time: "16:00 - 17:30",
-          title: "Student poster session",
-          speaker: "",
-          anchor: "",
-          isBreak: false
-        },
-        {
-          time: "17:30 - 19:00",
-          title: "Public lecture: Trustworthy AI for Clinical Decision Making",
-          speaker: "Prof. Rajeev Alur",
-          anchor: "rajeev-alur",
-          isBreak: false
+            day: "Friday, 11 December 2026",
+            sessions: [
+                {
+                    time: "09:30 - 12:30",
+                    title: "Complexity & Cryptography",
+                    speaker: "",
+                    anchor: "",
+                    isBreak: false
+                },
+                {
+                    time: "12:30 - 14:00",
+                    title: "Lunch",
+                    speaker: "",
+                    anchor: "",
+                    isBreak: true
+                },
+                {
+                    time: "14:00 - 17:30",
+                    title: "AI, ML & Quantum",
+                    speaker: "",
+                    anchor: "",
+                    isBreak: false
+                },
+                {
+                    time: "17:30 - 18:00",
+                    title: "Closing session",
+                    speaker: "",
+                    anchor: "",
+                    isBreak: false
+                }
+            ]
         }
-      ]
-    },
-    {
-      day: "Friday, 11 December 2026",
-      sessions: [
+    ],
+    speakers: [
         {
-          time: "09:30 - 12:30",
-          title: "Complexity & Cryptography",
-          speaker: "",
-          anchor: "",
-          isBreak: false
+            anchor: "rajeev-alur",
+            name: "Rajeev Alur",
+            affiliation: "University of Pennsylvania",
+            image: "assets/rajeev-alur.jpg",
+            website: "https://www.cis.upenn.edu/~alur/"
         },
         {
-          time: "12:30 - 14:00",
-          title: "Lunch",
-          speaker: "",
-          anchor: "",
-          isBreak: true
+            anchor: "ashutosh-trivedi",
+            name: "Ashutosh Trivedi",
+            affiliation: "University of Colorado Boulder",
+            image: "assets/ashutosh-trivedi.jpg",
+            website: "http://ashutoshtrivedi.com/"
         },
         {
-          time: "14:00 - 17:30",
-          title: "AI, ML & Quantum",
-          speaker: "",
-          anchor: "",
-          isBreak: false
+            anchor: "diptarka-chakraborty",
+            name: "Diptarka Chakraborty",
+            affiliation: "National University of Singapore",
+            image: "assets/diptarka-chakraborty.jpg",
+            website: "https://sites.google.com/view/diptarka"
         },
         {
-          time: "17:30 - 18:00",
-          title: "Closing session",
-          speaker: "",
-          anchor: "",
-          isBreak: false
+            anchor: "chandra-chekuri",
+            name: "Chandra Chekuri",
+            affiliation: "University of Illinois Urbana-Champaign",
+            image: "assets/chandra-chekuri.jpg",
+            website: "https://chekuri.cs.illinois.edu/"
+        },
+        {
+            anchor: "aditya-gopalan",
+            name: "Aditya Gopalan",
+            affiliation: "Indian Institute of Science, Bengaluru",
+            image: "assets/aditya-gopalan.jpg",
+            website: "https://ece.iisc.ac.in/~aditya/"
+        },
+        {
+            anchor: "preeti-rao",
+            name: "Preeti Rao",
+            affiliation: "Indian Institute of Technology Bombay",
+            image: "assets/preeti-rao.webp",
+            website: "https://www.ee.iitb.ac.in/people/faculty/preeti-rao/"
+        },
+        {
+            anchor: "devavrat-shah",
+            name: "Devavrat Shah",
+            affiliation: "Massachusetts Institute of Technology",
+            image: "assets/devavrat-shah.jpg",
+            website: "https://devavrat.mit.edu/"
+        },
+        {
+            anchor: "cm-chandrashekar",
+            name: "C M Chandrashekar",
+            affiliation: "Institute of Mathematical Sciences / TIFR",
+            image: "assets/cm-chandrashekar.jpg",
+            website: ""
+        },
+        {
+            anchor: "benny-applebaum",
+            name: "Benny Applebaum",
+            affiliation: "Tel Aviv University",
+            image: "assets/benny-applebaum.jpg",
+            website: "https://bennyapplebaum.sites.tau.ac.il/"
+        },
+        {
+            anchor: "srikanth-srinivasan",
+            name: "Srikanth Srinivasan",
+            affiliation: "University of Copenhagen",
+            image: "assets/srikanth-srinivasan.jpg",
+            website: "https://srikanth-srinivasan.bitbucket.io/"
+        },
+        {
+            anchor: "rohit-gurjar",
+            name: "Rohit Gurjar",
+            affiliation: "Indian Institute of Technology Bombay",
+            image: "assets/rohit-gurjar.jpg",
+            website: "https://www.cse.iitb.ac.in/~rgurjar/"
         }
-      ]
-    }
-  ],
-  speakers: [
-    {
-      anchor: "rajeev-alur",
-      name: "Rajeev Alur",
-      affiliation: "University of Pennsylvania",
-      image: "assets/rajeev-alur.jpg",
-      website: "https://www.cis.upenn.edu/~alur/"
-    },
-    {
-      anchor: "ashutosh-trivedi",
-      name: "Ashutosh Trivedi",
-      affiliation: "University of Colorado Boulder",
-      image: "assets/ashutosh-trivedi.jpg",
-      website: "http://ashutoshtrivedi.com/"
-    },
-    {
-      anchor: "diptarka-chakraborty",
-      name: "Diptarka Chakraborty",
-      affiliation: "National University of Singapore",
-      image: "assets/diptarka-chakraborty.jpg",
-      website: "https://sites.google.com/view/diptarka"
-    },
-    {
-      anchor: "chandra-chekuri",
-      name: "Chandra Chekuri",
-      affiliation: "University of Illinois Urbana-Champaign",
-      image: "assets/chandra-chekuri.jpg",
-      website: "https://chekuri.cs.illinois.edu/"
-    },
-    {
-      anchor: "aditya-gopalan",
-      name: "Aditya Gopalan",
-      affiliation: "Indian Institute of Science, Bengaluru",
-      image: "assets/aditya-gopalan.jpg",
-      website: "https://ece.iisc.ac.in/~aditya/"
-    },
-    {
-      anchor: "preeti-rao",
-      name: "Preeti Rao",
-      affiliation: "Indian Institute of Technology Bombay",
-      image: "assets/preeti-rao.webp",
-      website: "https://www.ee.iitb.ac.in/people/faculty/preeti-rao/"
-    },
-    {
-      anchor: "devavrat-shah",
-      name: "Devavrat Shah",
-      affiliation: "Massachusetts Institute of Technology",
-      image: "assets/devavrat-shah.jpg",
-      website: "https://devavrat.mit.edu/"
-    },
-    {
-      anchor: "cm-chandrashekar",
-      name: "C M Chandrashekar",
-      affiliation: "IISc Bengaluru",
-      image: "assets/cm-chandrashekar.jpg",
-      website: "https://eecs.iisc.ac.in/people/c-m-chandrashekar/"
-    },
-    {
-      anchor: "benny-applebaum",
-      name: "Benny Applebaum",
-      affiliation: "Tel Aviv University",
-      image: "assets/benny-applebaum.jpg",
-      website: "https://bennyapplebaum.sites.tau.ac.il/"
-    },
-    {
-      anchor: "srikanth-srinivasan",
-      name: "Srikanth Srinivasan",
-      affiliation: "University of Copenhagen",
-      image: "assets/srikanth-srinivasan.jpg",
-      website: "https://srikanth-srinivasan.bitbucket.io/"
-    },
-    {
-      anchor: "rohit-gurjar",
-      name: "Rohit Gurjar",
-      affiliation: "Indian Institute of Technology Bombay",
-      image: "assets/rohit-gurjar.jpg",
-      website: "https://www.cse.iitb.ac.in/~rgurjar/"
-    }
-  ]
+    ]
 };
--- js/programme.js
+++ js/programme.js
@@ -1,100 +1,142 @@
 (function () {
-  const scheduleMount = document.getElementById("schedule-mount");
-  const speakersMount = document.getElementById("speakers-mount");
+	const scheduleMount = document.getElementById("schedule-mount");
+	const speakersMount = document.getElementById("speakers-mount");
 
-  if (!scheduleMount || !speakersMount || typeof workshopData === "undefined") {
-    return;
-  }
+	if (!scheduleMount || !speakersMount || typeof workshopData === "undefined") {
+		return;
+	}
 
-  // 1. Render Timetable Schedule
-  let scheduleHTML = "";
-  workshopData.schedule.forEach(function (dayGroup) {
-    scheduleHTML += `
-      <div class="day-group">
-        <div class="day-title">${dayGroup.day}</div>
-        <table class="timetable">
-          <tbody>
-    `;
+	// 1. Render Timetable Schedule
+	let scheduleHTML = "";
+	workshopData.schedule.forEach(function (dayGroup) {
+		scheduleHTML += `
+			<div class="day-group">
+				<div class="day-title">${dayGroup.day}</div>
+				<table class="timetable">
+					<tbody>
+		`;
 
-    dayGroup.sessions.forEach(function (session) {
-      const rowClass = session.isBreak ? ' class="break-row"' : "";
-      let speakerLine = "";
+		dayGroup.sessions.forEach(function (session) {
+			const rowClass = session.isBreak ? ' class="break-row"' : "";
+			let speakerLine = "";
 
-      if (session.speaker) {
-        if (session.anchor) {
-          speakerLine = `<div class="timetable-speaker">Speaker: <a href="#speaker-${session.anchor}" class="speaker-jump-link">${session.speaker}</a></div>`;
-        } else {
-          speakerLine = `<div class="timetable-speaker">Speaker: ${session.speaker}</div>`;
-        }
-      }
+			if (session.speaker) {
+				if (session.anchor) {
+					speakerLine = `<div class="timetable-speaker">Speaker: <a href="#speaker-${session.anchor}" class="speaker-jump-link">${session.speaker}</a></div>`;
+				} else {
+					speakerLine = `<div class="timetable-speaker">Speaker: ${session.speaker}</div>`;
+				}
+			}
 
-      scheduleHTML += `
-        <tr${rowClass}>
-          <td class="timetable-time">${session.time}</td>
-          <td class="timetable-content">
-            <div class="timetable-title">${session.title}</div>
-            ${speakerLine}
-          </td>
-        </tr>
-      `;
-    });
+			let talksHTML = "";
+            if (session.talks && session.talks.length > 0) {
+                talksHTML += '<div class="subtalks-list">';
+                session.talks.forEach(function (talk) {
+                    let talkSpeaker = "";
+                    if (talk.speaker) {
+                        if (talk.anchor) {
+                            talkSpeaker = `<span class="subtalk-speaker">&mdash; <a href="#speaker-${talk.anchor}" class="speaker-jump-link">${talk.speaker}</a></span>`.replace("&mdash;", "-");
+                        } else {
+                            talkSpeaker = `<span class="subtalk-speaker">- ${talk.speaker}</span>`;
+                        }
+                    }
 
-    scheduleHTML += `
-          </tbody>
-        </table>
-      </div>
-    `;
-  });
-  scheduleMount.innerHTML = scheduleHTML;
+                    let abstractBlock = "";
+                    if (talk.abstract && talk.abstract.trim() !== "") {
+                        abstractBlock = `
+                            <details class="subtalk-abstract-details">
+                                <summary>Abstract</summary>
+                                <div class="subtalk-abstract-body">${talk.abstract}</div>
+                            </details>
+                        `;
+                    }
 
-  // 2. Render Confirmed Speakers Directory
-  // 2. Render Confirmed Speakers Directory
-  let speakersHTML = "";
-  workshopData.speakers.forEach(function (sp) {
-    let websitePill = "";
-    if (sp.website && sp.website.trim() !== "") {
-      websitePill = `
-        <a href="${sp.website}" class="speaker-web-pill" target="_blank" rel="noopener noreferrer" aria-label="Visit academic webpage of ${sp.name}">
-          <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
-            <circle cx="12" cy="12" r="10"></circle>
-            <line x1="2" y1="12" x2="22" y2="12"></line>
-            <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
-          </svg>
-          <span>Webpage</span>
-        </a>
-      `;
-    }
+                    let talkTime = (talk.time && talk.time.trim() !== "")
+                        ? `<span class="subtalk-time">${talk.time}</span>`
+                        : "";
 
-    speakersHTML += `
-      <article id="speaker-${sp.anchor}" class="speaker-card">
-        <img class="speaker-photo" src="${sp.image}" alt="Photo of ${sp.name}" loading="lazy">
-        <div class="speaker-name">${sp.name}</div>
-        <div class="speaker-affiliation">${sp.affiliation}</div>
-        ${websitePill}
-      </article>
-    `;
-  });
-  speakersMount.innerHTML = speakersHTML;
+                    talksHTML += `
+                        <div class="subtalk-item">
+                            <div class="subtalk-header">
+                                ${talkTime}
+                                <span class="subtalk-title"><strong>${talk.title}</strong></span>
+                                ${talkSpeaker}
+                            </div>
+                            ${abstractBlock}
+                        </div>
+                    `;
+                });
+                talksHTML += '</div>';
+            }
 
-  // 3. Highlight speaker card when clicked from schedule
-  document.addEventListener("click", function (event) {
-    const link = event.target.closest(".speaker-jump-link");
-    if (!link) return;
+            scheduleHTML += `
+                <tr${rowClass}>
+                    <td class="timetable-time">${session.time}</td>
+                    <td class="timetable-content">
+                        <div class="timetable-title${session.talks && session.talks.length ? ' umbrella-title' : ''}">${session.title}</div>
+                        ${speakerLine}
+                        ${talksHTML}
+                    </td>
+                </tr>
+            `;
+		});
 
-    const targetId = link.getAttribute("href").substring(1);
-    const targetCard = document.getElementById(targetId);
+		scheduleHTML += `
+					</tbody>
+				</table>
+			</div>
+		`;
+	});
+	scheduleMount.innerHTML = scheduleHTML;
 
-    if (targetCard) {
-      document.querySelectorAll(".speaker-card.highlighted").forEach(function (card) {
-        card.classList.remove("highlighted");
-      });
+	// 2. Render Confirmed Speakers Directory
+	// 2. Render Confirmed Speakers Directory
+	let speakersHTML = "";
+	workshopData.speakers.forEach(function (sp) {
+		let websitePill = "";
+		if (sp.website && sp.website.trim() !== "") {
+			websitePill = `
+				<a href="${sp.website}" class="speaker-web-pill" target="_blank" rel="noopener noreferrer" aria-label="Visit academic webpage of ${sp.name}">
+					<svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
+						<circle cx="12" cy="12" r="10"></circle>
+						<line x1="2" y1="12" x2="22" y2="12"></line>
+						<path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+					</svg>
+					<span>Webpage</span>
+				</a>
+			`;
+		}
 
-      targetCard.classList.add("highlighted");
-      targetCard.scrollIntoView({ behavior: "smooth", block: "center" });
+		speakersHTML += `
+			<article id="speaker-${sp.anchor}" class="speaker-card">
+				<img class="speaker-photo" src="${sp.image}" alt="Photo of ${sp.name}" loading="lazy">
+				<div class="speaker-name">${sp.name}</div>
+				<div class="speaker-affiliation">${sp.affiliation}</div>
+				${websitePill}
+			</article>
+		`;
+	});
+	speakersMount.innerHTML = speakersHTML;
 
-      setTimeout(function () {
-        targetCard.classList.remove("highlighted");
-      }, 2500);
-    }
-  });
+	// 3. Highlight speaker card when clicked from schedule
+	document.addEventListener("click", function (event) {
+		const link = event.target.closest(".speaker-jump-link");
+		if (!link) return;
+
+		const targetId = link.getAttribute("href").substring(1);
+		const targetCard = document.getElementById(targetId);
+
+		if (targetCard) {
+			document.querySelectorAll(".speaker-card.highlighted").forEach(function (card) {
+				card.classList.remove("highlighted");
+			});
+
+			targetCard.classList.add("highlighted");
+			targetCard.scrollIntoView({ behavior: "smooth", block: "center" });
+
+			setTimeout(function () {
+				targetCard.classList.remove("highlighted");
+			}, 2500);
+		}
+	});
 })();
